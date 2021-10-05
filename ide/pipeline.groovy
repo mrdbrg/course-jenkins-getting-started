57db3be30b855db1ea1e166fe268f1f5dcf3cb41
mrdbrg <rdg.brg@gmail.com>
2021-10-05 20:08:32 -0300

    final refactor to buld process with notifications on changed build status only

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -20,14 +20,15 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-
-                    emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input", 
-                    body: "Pleas go to ${BUILD_URL} and verify the build", 
-                    compressLog: true, 
-                    attachLog: true,
-                    to: "test@jenkins",
-                    recipientProviders: [upstreamDevelopers(), requestor()]
                 }
+                changed {
+                        emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) ${currentBuild.result}", 
+                        body: "Pleas go to ${BUILD_URL} and verify the build", 
+                        compressLog: true, 
+                        attachLog: true,
+                        to: "test@jenkins",
+                        recipientProviders: [upstreamDevelopers(), requestor()]
+                    }
             }
         }
     }
